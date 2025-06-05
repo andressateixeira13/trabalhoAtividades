3d4e19a0ee630f05788ac129edc0d593d07e8d45
andressateixeira13 <andressateixeira380@gmail.com>
2025-06-04 23:25:12 -0300

    ajustes frontend

--- backend_atividades/src/main/java/com/example/backend_atividades/models/Feedback.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend_atividades.models;
-
-import java.awt.*;
-
-public class Feedback {
-    private Long codFeedback;
-    private TextArea descricao;
-}
--- backend_atividades/src/main/java/com/example/backend_atividades/repositories/RetornoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.backend_atividades.repositories;
-
-import com.example.backend_atividades.models.Retorno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RetornoRepository extends JpaRepository<Retorno, Long> {
-}
